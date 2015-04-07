dc8d1562da8c1635d1a306bbbac9425fec041a5e
Peter Mauldin <petermauldin1@gmail.com>
2015-04-07 12:01:56 -0500

    Updated tests 1 & 2 to improve readability [used function variables rather than inline funtions for afterJSON()]

--- clyp_spec.js
+++ clyp_spec.js
@@ -12,12 +12,9 @@ var title = 'Register';
 var description = 'POST Test';
 var longitude = -97;
 var latitude = 30;
-
-var playlistID;
-var playlistUploadToken;
 //
 
-// URL Parameters
+//////// Test #1 URL Parameters /////////
 form.append('audioFile', fs.createReadStream(filePath), {
   knownLength: fs.statSync(filePath).size         // we need to set the knownLength so we can call  form.getLengthSync()
 });
@@ -26,8 +23,10 @@ form.append('description', 'POST Test');
 form.append('title', title);
 form.append('longitude', longitude);
 form.append('latitude', latitude);
-//
+/////////////////////////////////
 
+
+// Test #1 - POST Audio to Clyp.it upload staging
 frisby.create('POST audio to https://uploadstaging.clyp.it/upload')
   .post(UPLOAD_URL+ '/upload',
   form,
@@ -50,6 +49,7 @@ frisby.create('POST audio to https://uploadstaging.clyp.it/upload')
     PlaylistUploadToken: String
   })
   .expectJSON({
+    // Assert that all the fields we passed in were correctly returned
     Successful: true,
     Description: description,
     Title: title,
@@ -57,50 +57,61 @@ frisby.create('POST audio to https://uploadstaging.clyp.it/upload')
     Latitude: latitude
   })  
   //.inspectJSON() // Prints out response to console
-  .afterJSON(function(json) {
+  .afterJSON(test2) // If Test #1 Passed, run Test #2 with JSON response from Test #1
+.toss();
+/* End Test #1 */
+
+
+
+//Test #2 - POST Audio to Playlist created by Test #1
+function test2(json) {
+
     // First Test Passed, Retrieve PlaylistId and Token
+    var playlistID = json.PlaylistId;
+    var playlistUploadToken = json.PlaylistUploadToken;
 
-    playlistID = json.PlaylistId;
-    playlistUploadToken = json.PlaylistUploadToken;
-
-    // Test #2 - POST Audio to existing Playlist
-
-    var form2 = new FormData();
+    // Set variables for new test
+    form = new FormData(); // Wipe Multi-Part Form-Data clean 
     filePath = path.resolve(__dirname, 'gun.mp3');
     title = 'gun';
     
-    form2.append('audioFile', fs.createReadStream(filePath), {
+    // New URL Parameters
+    form.append('audioFile', fs.createReadStream(filePath), {
       knownLength: fs.statSync(filePath).size         // we need to set the knownLength so we can call  form.getLengthSync()
     });
 
-    form2.append('title', title);
-    form2.append('PlaylistId', playlistID);
-    form2.append('PlaylistUploadToken', playlistUploadToken);
+    form.append('title', title);
+    form.append('PlaylistId', playlistID);
+    form.append('PlaylistUploadToken', playlistUploadToken);
 
     frisby.create('Add new audio file to playlist we created')
       .post(UPLOAD_URL+ '/upload',
-      form2,
+      form,
       {
         json: false,
         headers: {
           // Add headers for POSTing Audio
-          'content-type': 'multipart/form-data; boundary=' + form2.getBoundary(),
-          'content-length': form2.getLengthSync()
+          'content-type': 'multipart/form-data; boundary=' + form.getBoundary(),
+          'content-length': form.getLengthSync()
         }
       })
       //.inspectJSON()
       .expectStatus(200)
       .expectJSONTypes({
+        Successful: Boolean,
+        Title: String,
         PlaylistId: String,
         PlaylistUploadToken: String
       })
       .expectJSON({
-        PlaylistId: playlistID,
+        Successful: true,
+        Title: title,
+        PlaylistId: playlistID, // Make sure the playlistID that is returned is the same as in Test #1
         PlaylistUploadToken: playlistUploadToken
       })
     .toss();
-  })
-.toss();
+};
+/* End Test #2 */
 
 
 
